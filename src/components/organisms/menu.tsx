3ca8f380cc1c9f18be49c423155f1c8568d660f6
keigrey <keixgrey@gmail.com>
2022-10-31 19:16:49 +0900

    feat: Add Menu component

---
+++ src/components/organisms/menu.tsx
@@ -0,0 +1,13 @@
+export default function Menu() {
+  return (
+    <ul className="menu">
+      <li>hm</li>
+      <li>about</li>
+      <li>resume</li>
+      <li>projects</li>
+      <li>hobbies</li>
+      <li>mail</li>
+      <li>info</li>
+    </ul>
+  );
+}
