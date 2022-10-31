8fe882cd888cbb494fc9639b878a00833673257e
keigrey <keixgrey@gmail.com>
2022-10-31 19:18:31 +0900

    feat: Add components to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "./styles/Home.module.css";
+import Name from "../components/atoms/name";
+import MainTitle from "../components/atoms/mainTitle";
+import Button from "../components/atoms/button";
+import Menu from "../components/organisms/menu";
+import Moon from "../components/atoms/moon";
+import CloudWave from "../components/molecules/cloudWave";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>SERGEI KOSHELEV</h1>Home Page
+      <Name text="SERGEI KOSHELEV" />
+      <MainTitle text="FULLSTACK SOFTWARE ENGINEER" />
+      <Button text="NIGHT" />
+      <Menu />
+      <Moon />
+      <CloudWave />
     </div>
   );
 }
