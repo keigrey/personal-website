b381a7fe04f6e11a8f6bad707723c018ba97925e
keigrey <keixgrey@gmail.com>
2022-11-01 18:54:13 +0900

    feat: Add new Button components

--- src/components/templates/leftContainer.tsx
+++ src/components/templates/leftContainer.tsx
@@ -1,15 +1,19 @@
 import Name from "../atoms/name";
 import MainTitle from "../atoms/mainTitle";
-import Button from "../atoms/button";
+import ButtonDark from "../atoms/buttonDark";
+import ButtonLight from "../atoms/buttonLight";
 import Menu from "../organisms/menu";
+import LanguageSwitch from "../atoms/languageSwitch";
 
 export default function LeftContainer() {
   return (
     <div id="left-container">
       <div id="left-sub-container">
+        <LanguageSwitch />
         <Name text="SERGEI KOSHELEV" />
         <MainTitle text="FULLSTACK SOFTWARE ENGINEER" />
-        <Button text="NIGHT" />
+        <ButtonDark text="NIGHT" />
+        <ButtonLight text="DAY" />
         <Menu />
       </div>
     </div>
