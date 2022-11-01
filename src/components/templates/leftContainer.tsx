2ec991209f2eb555651cf32a96200f07e373d7e9
keigrey <keixgrey@gmail.com>
2022-11-01 17:56:46 +0900

    feat: Add sub-container div to LeftContainer

--- src/components/templates/leftContainer.tsx
+++ src/components/templates/leftContainer.tsx
@@ -5,11 +5,13 @@ import Menu from "../organisms/menu";
 
 export default function LeftContainer() {
   return (
-    <div className="left-container">
-      <Name text="SERGEI KOSHELEV" />
-      <MainTitle text="FULLSTACK SOFTWARE ENGINEER" />
-      <Button text="NIGHT" />
-      <Menu />
+    <div id="left-container">
+      <div id="left-sub-container">
+        <Name text="SERGEI KOSHELEV" />
+        <MainTitle text="FULLSTACK SOFTWARE ENGINEER" />
+        <Button text="NIGHT" />
+        <Menu />
+      </div>
     </div>
   );
 }
