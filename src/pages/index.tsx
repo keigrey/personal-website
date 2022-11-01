3964673ef626d77e1040a1795084e1a303891513
keigrey <keixgrey@gmail.com>
2022-11-01 16:51:00 +0900

    feat: Add Left and  RightContainer to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,11 @@
-import styles from "./styles/Home.module.css";
-import Name from "../components/atoms/name";
-import MainTitle from "../components/atoms/mainTitle";
-import Button from "../components/atoms/button";
-import Menu from "../components/organisms/menu";
-import Moon from "../components/atoms/moon";
-import CloudWave from "../components/molecules/cloudWave";
+import LeftContainer from "../components/templates/leftContainer";
+import RightContainer from "../components/templates/rightContainer";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Name text="SERGEI KOSHELEV" />
-      <MainTitle text="FULLSTACK SOFTWARE ENGINEER" />
-      <Button text="NIGHT" />
-      <Menu />
-      <Moon />
-      <CloudWave />
+    <div id="container">
+      <LeftContainer />
+      <RightContainer />
     </div>
   );
 }
