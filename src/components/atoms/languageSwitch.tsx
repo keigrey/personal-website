48f30c05bdcb5cf4ae3056276d39e9566564d92c
keigrey <keixgrey@gmail.com>
2022-11-01 18:53:49 +0900

    feat: Add LanguageSwitch component

---
+++ src/components/atoms/languageSwitch.tsx
@@ -0,0 +1,9 @@
+export default function LanguageSwitch() {
+  return (
+    <div className="language-selector">
+      <div className="language-outline">
+        <h3 className="language-text">en</h3>
+      </div>
+    </div>
+  );
+}
